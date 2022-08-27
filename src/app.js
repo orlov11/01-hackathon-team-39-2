d21bd9bb35b58ea7c81fcbdccce977b1495ac102
nonfermata <nonfermata@gmail.com>
2022-08-27 11:06:28 +0400

    made clicks-module

--- src/app.js
+++ src/app.js
@@ -1,2 +1,6 @@
 import './styles.css'
+import { ClicksModule } from './modules/clicks.module'
 
+const myClicks = new ClicksModule('some-type', 'Считать клики (за 3 секунды)')
+alert('Lets start!')
+myClicks.trigger()
--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,32 @@
 import {Module} from '../core/module'
+import './clicks-styles.css'
 
 export class ClicksModule extends Module {
+    constructor(type, text) {
+        super(type, text)
+    }
+    trigger() {
+        const timeStatusBlock = document.createElement('div')
+        timeStatusBlock.className = 'time-status'
+        timeStatusBlock.textContent = 'Start'
+        document.body.append(timeStatusBlock)
 
+        let clicks = 0
+        const currentTime = Date.now()
+        const counterBlock = document.createElement('div')
+        counterBlock.className = 'clicks-counter'
+        counterBlock.innerHTML = `You have clicked <b></b> times.`
+        const clickNumberBlock = counterBlock.querySelector('b')
+        clickNumberBlock.textContent = clicks
+        document.body.append(counterBlock)
+
+        document.addEventListener('click', () => {
+            if (Date.now() - currentTime < 3000) {
+                clicks += 1
+                clickNumberBlock.textContent = clicks
+            } else {
+                timeStatusBlock.textContent = 'End'
+            }
+        })
+    }
 }
