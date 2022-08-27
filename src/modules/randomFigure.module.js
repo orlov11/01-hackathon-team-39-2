85612e3dce6dccc9c6444e1097702c84567611cf
Valya <benedict.1@yandex.ru>
2022-08-27 11:50:31 +0300

    edit git review

--- src/modules/randomFigure.module.js
+++ src/modules/randomFigure.module.js
@@ -1,5 +1,5 @@
 import {Module} from '../core/module'
-
+import {random} from "../utils"
 export class RandomFigure extends Module {
   
     constructor(type, text) {
@@ -7,37 +7,22 @@ export class RandomFigure extends Module {
     }  
 
     trigger() {
-    const figure = document.querySelector(".figure")
+        const figure = document.querySelector(".figure")
         if (figure) {
         setTimeout(() => figure.remove(), 200);
-    }
-    
-    circleRun()
-    }
-}
-function circleRun(){
-    const el = renderFigure()
-    document.body.appendChild(el);
-} 
-
-
-function renderFigure(){
-    const w = window.innerWidth;
-    const h = window.innerHeight;
-    let size = (50 + 50 * Math.random()) | 0;
-    let color = (0xFFFFFF * Math.random()) | 0x1000000;
-    let div = document.createElement('div');
-    div.classList.add("figure");
-    div.style.position ="absolute" ;
-    div.style.width = getRandomInRange(50, 150) + 'px';
-    div.style.height = getRandomInRange(50, 150) + 'px';
-    div.style.borderRadius = getRandomInRange(1, 50) +'%';
-    div.style.backgroundColor = '#' + (Math.random().toString(16) + '000000').substring(2,8).toUpperCase()
-    div.style.top = Math.random() * (h - size) + 'px';
-    div.style.left = Math.random() * (w - size) + 'px';
-    console.log(div)
-    return div 
-}
-function getRandomInRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        let size = (50 + 50 * Math.random()) | 0;
+        let div = document.createElement('div');
+        div.classList.add("figure");
+        div.style.position ="absolute" ;
+        div.style.width = random(50, 150) + 'px';
+        div.style.height = random(50, 150) + 'px';
+        div.style.borderRadius = random(1, 50) +'%';
+        div.style.backgroundColor = `rgb(${random(0,256)}, ${random(0,256)}, ${random(0,256)}`
+        div.style.top = Math.random() * (height - size) + 'px';
+        div.style.left = Math.random() * (width - size) + 'px';
+        document.body.appendChild(div);
+        }
 }
