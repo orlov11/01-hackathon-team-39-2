be653df3e73711c1731bbca2718cba92e0d2ec8b
Pavel Ynyshev <paha020798@mail.ru>
2022-08-27 17:21:32 +0700

    Implemented background module_2

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,15 +1,12 @@
 import {Module} from '../core/module'
 import { random } from "./../utils"
 
-const randomColorRGB = () =>
-`rgb(${random(0, 256)}, ${random(0, 256)}, ${random(0, 256)})`
-
 export class BackgroundModule extends Module {
   constructor(type,text) {
     super(type, text)
   }
-  trigger()  {
-    document.body.style.backgroundColor = randomColorRGB()
+  randomColorRGB()  {
+    document.body.style.backgroundColor = `rgb(${random(0, 256)}, ${random(0, 256)}, ${random(0, 256)})`
     document.body.style.transition = `background-color 500ms ease`
   }
 }
